0f15b1aa0ef421a4f3d4de82b1a153a098504607
Eloy Duran <eloy.de.enige@gmail.com>
2009-08-17 21:04:17 +0200

    Post a gist

--- lib/cocoa_gist.rb
+++ lib/cocoa_gist.rb
@@ -1,10 +1,36 @@
 require 'osx/cocoa'
+require 'cgi'
 
 class CocoaGist < OSX::NSObject
+  REQUEST_URL = OSX::NSURL.URLWithString('http://gist.github.com/gists')
+  TIMEOUT = 10
+  POLICY = 1 # NSURLRequestReloadIgnoringLocalCacheData
+  
+  attr_reader :connection
+  
   def self.credentials
     @credentials ||= {
-      :user =>  `git config --global github.user`.strip,
-      :token => `git config --global github.token`.strip
-    }
+      'login' => `git config --global github.user`.strip,
+      'token' => `git config --global github.token`.strip
+    }.reject { |_, v| v.empty? }
+  end
+  
+  def start(content, syntax = 'ruby')
+    request = post_request(params(content))
+    @connection = OSX::NSURLConnection.alloc.initWithRequest_delegate(request, self)
+  end
+  
+  private
+  
+  def post_request(body)
+    request = OSX::NSMutableURLRequest.requestWithURL_cachePolicy_timeoutInterval(REQUEST_URL, POLICY, TIMEOUT)
+    request.setHTTPMethod('POST')
+    request.setHTTPBody(OSX::NSData.dataWithRubyString(body))
+    request
+  end
+  
+  def params(content)
+    params = { 'file_contents[gistfile1]' => content }.merge(self.class.credentials)
+    params.inject('') { |body, (key, value)| body << "#{key}=#{CGI.escape(value)}&" }.chop
   end
 end
--- test/cocoa_gist_test.rb
+++ test/cocoa_gist_test.rb
@@ -6,6 +6,8 @@ require "mocha"
 $:.unshift File.expand_path('../../lib', __FILE__)
 require "cocoa_gist"
 
+GITHUB_ACCOUNT = { 'login' => 'alloy', 'token' => 'secret' }
+
 describe 'CocoaGist' do
   it "should lazy load the github user info once and return it as a hash" do
     CocoaGist.instance_variable_set(:@credentials, nil)
@@ -13,7 +15,51 @@ describe 'CocoaGist' do
     CocoaGist.expects(:`).with('git config --global github.token').returns("secret\n").times(1)
     
     3.times do
-      CocoaGist.credentials.should == { :user => 'alloy', :token => 'secret' }
+      CocoaGist.credentials.should == GITHUB_ACCOUNT
     end
+  end
+  
+  it "should return an empty hash if no account info was found" do
+    CocoaGist.instance_variable_set(:@credentials, nil)
+    CocoaGist.stubs(:`).returns('')
+    CocoaGist.credentials.should == {}
+  end
+end
+
+describe "A CocoaGist" do
+  before do
+    CocoaGist.instance_variable_set(:@credentials, GITHUB_ACCOUNT)
+    
+    @gist = CocoaGist.alloc.init
+  end
+  
+  it "should serialize the parameters" do
+    @gist.send(:params, 'the content').split('&').sort.should ==
+      %w{ file_contents[gistfile1]=the+content  login=alloy  token=secret }.sort
+  end
+  
+  it "should omit empty values from the serialized parameters" do
+    CocoaGist.stubs(:credentials).returns({})
+    @gist.send(:params, 'the content').should == 'file_contents[gistfile1]=the+content'
+  end
+  
+  it "should post the paste contents" do
+    @gist.expects(:params).with('the content').returns('the parameters')
+    
+    request = mock('NSMutableURLRequest')
+    OSX::NSMutableURLRequest.expects(:requestWithURL_cachePolicy_timeoutInterval).with do |url, policy, timeout|
+      url.absoluteString == 'http://gist.github.com/gists' && policy == 1 && timeout == 10
+    end.returns(request)
+    
+    request.expects(:setHTTPMethod).with('POST')
+    request.expects(:setHTTPBody).with do |body|
+      body.rubyString == 'the parameters'
+    end
+    
+    connection = mock('NSURLConnection')
+    OSX::NSURLConnection.any_instance.expects(:initWithRequest_delegate).with(request, @gist).returns(connection)
+    
+    @gist.start('the content')
+    @gist.connection.should.be connection
   end
 end
